66b0753948103f9e6567484e9119ddc3faff7303
Alex <vasincaalex@gmail.com>
2016-11-20 15:54:04 +0200

    Initial commit

---
+++ services/wit.js
@@ -0,0 +1,112 @@
+'use strict'
+
+var Config = require('../config')
+var FB = require('../connectors/facebook')
+var Wit = require('node-wit').Wit
+var request = require('request')
+
+
+var firstEntityValue = function (entities, entity) {
+	var val = entities && entities[entity] &&
+		Array.isArray(entities[entity]) &&
+		entities[entity].length > 0 &&
+		entities[entity][0].value
+
+	if (!val) {
+		return null
+	}
+	return typeof val === 'object' ? val.value : val
+}
+
+
+var actions = {
+	say (sessionId, context, message, cb) {
+		// Bot testing mode, run cb() and return
+		if (require.main === module) {
+			cb()
+			return
+		}
+
+		console.log('WIT WANTS TO TALK TO:', context._fbid_)
+		console.log('WIT HAS SOMETHING TO SAY:', message)
+		console.log('WIT HAS A CONTEXT:', context)
+
+		if (checkURL(message)) {
+			FB.newMessage(context._fbid_, message, true)
+		} else {
+			FB.newMessage(context._fbid_, message)
+		}
+
+		
+		cb()
+		
+	},
+
+	merge(sessionId, context, entities, message, cb) {
+		// Reset the weather story
+		delete context.Salut
+
+		// Retrive the location entity and store it in the context field
+		var loc = firstEntityValue(entities, 'buletin')
+		if (loc) {
+			context.loc = loc
+		}
+
+		// Reset the cutepics story
+		delete context.pics
+
+		// Retrieve the category
+		var category = firstEntityValue(entities, 'PFA')
+		if (category) {
+			context.cat = category
+		}
+
+		// Retrieve the sentiment
+		var sentiment = firstEntityValue(entities, 'sentiment')
+		if (sentiment) {
+			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
+		} else {
+			delete context.ack
+		}
+
+		cb(context)
+	},
+
+	error(sessionId, context, error) {
+		console.log(error.message)
+	},
+
+	// list of functions Wit.ai can execute
+	['getDocuments'](sessionId, context, cb) {
+
+		context.Salut = 'cerere tip pentru eliberarea actului de identitate, certificatul de naştere cu care se face dovada cetăţeniei române, original şi copie, actul de identitate al unuia dintre părinţi  '
+
+		cb(context)
+	},
+
+}
+
+// SETUP THE WIT.AI SERVICE
+var getWit = function () {
+	console.log('GRABBING WIT')
+	return new Wit(Config.WIT_TOKEN, actions)
+}
+
+module.exports = {
+	getWit: getWit,
+}
+
+// BOT TESTING MODE
+if (require.main === module) {
+	console.log('Bot testing mode!')
+	var client = getWit()
+	client.interactive()
+}
+
+
+// CHECK IF URL IS AN IMAGE FILE
+var checkURL = function (url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
+
